d485935d69a56ba046777df8d4d951c68b394442
samm <sammura.mg@googlemail.com>
2019-04-06 14:54:40 +0100

    insert customer

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
+app.use(bodyParser.json());
 //sql library
 const pool = require('pg').Pool;
 //connect to database
@@ -46,6 +48,25 @@ app.get('/customer/:id', (req, res)=>{
             customer : result.rows[0]
         });
     });
+});
+
+// insert customer to the db
+app.post('/customer', (req, res)=>{
+    const customer = {
+        name : req.body.name,
+        email : req.body.email
+    };
+    const qur = 'insert into customers (name, email) values ($1, $2)returning id ';
+    db.query(qur, [customer.name, customer.email], (err, result) =>{
+        if(err == undefined){
+            const newId = result.rows[0].id;
+            res.status(200).json({
+                id : newId
+            });           
+        } else {
+            res.status(500).json({error : err})   
+        }
+    })
 });
 app.listen(3000, () =>{
     console.log(`server run on 3000`);
